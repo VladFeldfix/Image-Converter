5d4e6f3b7c0a66e469bcb8d4e8f2f85faf6a53b0
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2025-03-12 11:48:58 +0200

    Add files via upload

--- install.cmd
+++ install.cmd
@@ -1,3 +1,1 @@
-set "currentDirectory=%cd%
-pyinstaller --distpath %currentDirectory% -i favicon.ico --onefile --noconsole Image-Converter.py
-pause
+pyinstaller -i favicon.ico --onefile --noconsole Image-Converter.py
